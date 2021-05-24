3a0577139eed0b36db634e74aa2ecd3ba62b9b63
Nghia Pham Duc <mpdn99@gmail.com>
2021-05-25 00:50:23 +0700

    update2

--- src/components/Container/CartItem/index.js
+++ src/components/Container/CartItem/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 import NikeAirForce from '../../../images/air-force-1-07-shoe-3nGnlt.jpg'
 import {Plus, Minus, X} from 'react-feather'
 
 const CartItem = () => {
+    const [amount, setAmount] = useState(1);
+    const increaseAmount = () => {
+        setAmount(amount-1);
+    }
+    const decreaseAmount = () => {
+        setAmount(amount+1);
+    }
     return (
         <div className="cartitem">
             <img className="cartitem-img" src={NikeAirForce} />
@@ -12,11 +19,11 @@ const CartItem = () => {
                 <button className="sizeBtn">Size: 41</button>
             </div>
             <div className="cartitem-amount">
-                <button className="cartitem-amount-control">
+                <button className="cartitem-amount-control" onClick={increaseAmount} disabled={amount <= 1}>
                     <Minus />
                 </button>
-                <p>1</p>
-                <button className="cartitem-amount-control">
+                <p>{amount}</p>
+                <button className="cartitem-amount-control" onClick={decreaseAmount}>
                     <Plus />
                 </button>
             </div>
